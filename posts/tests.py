9a53956e716e71c1ccb59839579c5690ea528a94
Buzzlay <dimabuslaev@gmail.com>
2020-11-14 14:22:04 +0500

    edited

--- posts/tests.py
+++ posts/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, Client
-from django.urls import reverse_lazy
-from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 from posts.models import Post, User, Group
 
@@ -10,30 +9,38 @@ class StaticURLTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user = User.objects.create_user(username='dimabuslaev', email='dimabuslaev@mail.ru')
+        cls.user = User.objects.create_user(
+            username='dimabuslaev',
+            email='dimabuslaev@mail.ru'
+        )
         cls.authorized_client = Client()
         cls.authorized_client.force_login(cls.user)
         cls.unauthorized_client = Client()
 
     def test_homepage(self):
-        url = reverse_lazy('index')
+        url = reverse('index')
         response = self.unauthorized_client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_new_post(self):
-        group = Group.objects.create(title='testgroup', description='test description', slug=1)
+        group = Group.objects.create(
+            title='testgroup',
+            description='test description',
+            slug='1')
         response = self.authorized_client.post(
-            '/new/',
-            data={'text': 'текст публикации', 'group': group.slug},
+            reverse('new_post'),
+            data={'text': 'текст публикации', 'group': group.id},
             follow=True
         )
         self.assertEqual(response.status_code, 200)
-        post = get_object_or_404(Post, text='текст публикации')
+        post = Post.objects.first()
         self.assertEqual(post.text, 'текст публикации')
+        self.assertEqual(post.author.username, 'dimabuslaev')
+        self.assertEqual(post.group.title, 'testgroup')
 
     def test_unauthorized_user_newpage(self):
         response = self.unauthorized_client.get(
-            '/new/',
+            reverse('new_post'),
             follow=False
         )
         self.assertRedirects(
@@ -45,19 +52,58 @@ class StaticURLTests(TestCase):
 
     def test_profile_existence(self):
         self.authorized_client.force_login(self.user)
-        response = self.authorized_client.get(f'/{self.user.username}/')
+        response = self.authorized_client.get(
+            reverse(
+                'profile',
+                kwargs={'username': self.user.username}
+            )
+        )
         self.assertEqual(response.status_code, 200)
 
     def test_post_edit(self):
-        target_post = Post.objects.create(author=self.user, text='Текст публикации')
-        group = Group.objects.create(title='testgroup', description='test description', slug=1)
-        self.authorized_client.force_login(self.user)
-        response = self.authorized_client.post(
-            f'/{self.user}/{target_post.id}/edit/',
-            {'text': 'Этот текст публикации изменён', 'group': group.id},
-            follow=True
+        target_post = Post.objects.create(
+            author=self.user,
+            text='Текст публикации'
         )
+        group = Group.objects.create(
+            title='testgroup',
+            description='test description',
+            slug=1
+        )
+        response = self.authorized_client.post(
+            reverse(
+                'post_edit',
+                kwargs={
+                    'username': self.user.username,
+                    'post_id': target_post.id
+                }
+            ),
+            {'text': 'Этот текст публикации изменён', 'group': group.id},
+            follow=True)
         self.assertEqual(response.status_code, 200)
-        post = get_object_or_404(Post, author=self.user)
-        self.assertEqual(post.text, 'Этот текст публикации изменён')
-        self.assertEqual(post.group.title, 'testgroup')
+        target_post.refresh_from_db()
+        self.assertEqual(target_post.text, 'Этот текст публикации изменён')
+        self.assertEqual(group.title, 'testgroup')
+
+    def test_post_existence(self):
+        post = Post.objects.create(author=self.user,
+                                   text='Текст публикации')
+        response_index = self.authorized_client.get(reverse('index'))
+        response_profile = self.authorized_client.get(
+            reverse(
+                'profile',
+                kwargs={'username': self.user.username}
+            )
+        )
+        response_post = self.authorized_client.get(
+            reverse(
+                'post',
+                kwargs={
+                    'username': self.user.username,
+                    'post_id': post.id
+                }
+            )
+        )
+        self.assertContains(response_index, 'Текст публикации')
+        self.assertContains(response_profile, 'Текст публикации')
+        self.assertContains(response_post, 'Текст публикации')
